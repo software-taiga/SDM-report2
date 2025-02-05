a04e5f9ad22ed2778190b1fcdd34cca5b675cf65
software-taiga <yamamoto.taiga.wa@tut.jp>
2025-02-05 14:02:00 +0900

    テストケース作成

--- test_cases.py
+++ test_cases.py
@@ -20,3 +20,27 @@ class TestCalc (unittest.TestCase):
 
     def test_sample4(self):
         self.assertEqual(-1, calc(0.1, 999))
+
+    def test_sample5(self):
+        self.assertEqual(-1, calc(1, 999.1))
+
+    def test_sample6(self):
+        self.assertEqual(-1, calc('A', 999))
+
+    def test_sample7(self):
+        self.assertEqual(-1, calc(-1, 1))
+
+    def test_sample8(self):
+        self.assertEqual(-1, calc(0, 999))
+
+    def test_sample9(self):
+        self.assertEqual(998001, calc(999, 999))
+
+    def test_sampleq10(self):
+        self.assertEqual(-1, calc(500.5, 999))
+
+    def test_sample11(self):
+        self.assertEqual(751494, calc(998, 753))
+
+    def test_sample12(self):
+        self.assertEqual(250000, calc(500, 500))
